90b5a268ebffeb03f71acc6a61c8a59ed8f39327
kiwi-fruitiwi <zane.tian@gmail.com>
2021-10-18 12:44:08 -0700

    add template project

---
+++ sketch.js
@@ -0,0 +1,19 @@
+/*
+@author Kiwi
+@date 2021-10-18
+
+ */
+let font
+
+function preload() {
+    font = loadFont('fonts/Meiryo-01.ttf')
+}
+
+function setup() {
+    createCanvas(640, 360)
+    colorMode(HSB, 360, 100, 100, 100)
+}
+
+function draw() {
+    background(234, 34, 24)
+}
